317ad382ca9cae40818035f2d90de40b30a78fd8
Michael Nishizawa <mnishizawa@wolverine.(none)>
2010-08-05 20:52:30 -0400

    deleted some files

--- archive/dbupdate/dbupgrade/background_maker/image_scraper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCREEN_DIMENSIONS=1920x1200
-
-for i in `seq 1 175`;do
-  wget -U Mozilla -O /tmp/interfacelift.html http://interfacelift.com/wallpaper_beta/downloads/downloads/widescreen/${SCREEN_DIMENSIONS}/index${i}.html
-  grep -o "http://interfacelift.com/wallpaper_beta/previews/.*\.jpg" /tmp/interfacelift.html | sed -e s/previews/dl/g | sed s/\.jpg/"\_${SCREEN_DIMENSIONS}.jpg"/g | xargs wget --wait=20 -U Mozilla ;
-done
--- archive/dbupdate/dbupgrade/gmail.sh
+++
@@ -1,1 +0,0 @@
-/opt/google/chrome/google-chrome --user-data-dir="/home/mnishizawa/.config/google-chrome/personal" --app="https://mail.google.com/a/nishizawas.net/#"
