344449eb51c55df7db027b66fb82e23737b831e8
Michael Nishizawa <michael@nishizawas.net>
2010-08-05 23:57:23 -0400

    cleaned up the script a little, removed the debugging messages for publishing

--- incubator/worklog.sh
+++ incubator/worklog.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-DEBUG=1
-
 # Worklog polls the system for information on what you are actively working on at any given time.
+#  The script dumps output to STDOUT so it can be redirected to a log file or to the screen
+#
+#  DEPENDENCIES
+#    1. dbus - for the interface to gnome-screensaver
+#    2. gnome-screensaver - provides API for monitoring status
+#    3. xprop - gives the window name for active windows 
+#
+
+
 POLL_FREQUENCY=5
-stat=''
-last_stat=''
+stat='0'
 
-function debug 
-{
-    if [ $DEBUG -eq 0 ]; then 
-        echo $0 :: $1
-    fi
-}
-
-while [ 1 = 1 ]; do
+while [ true ]; do
     last_stat=$stat
 
+    #If gnome-screensaver is running, assume the system is idle
     xidle=$(dbus-send --session --dest=org.gnome.ScreenSaver --type=method_call --print-reply --reply-timeout=2000 /org/gnome/ScreenSaver org.gnome.ScreenSaver.GetActive | sed -nr 's/^.*boolean (.*)$/\1/p')
     if [ "true" = "$xidle" ]; then
         stat=0
@@ -24,24 +24,18 @@ while [ 1 = 1 ]; do
         stat=$(xprop -root | sed -nr 's/^_NET_ACTIVE_WINDOW.*\# (.*)/\1/p')
     fi
 
-    debug "stat is $stat"
-
+    # if the same app is running, do nothing
     if [ "${stat}" = "${last_stat}" ]; then
         sleep $POLL_FREQUENCY
         continue;
     fi
     
-    debug "${stat} != ${last_stat}"
-
     if [ "0" = "$stat" ]; then
-        debug "IDLE"
         app="idle"
         window_title=""
     else
         app=$(xprop -id ${stat} | sed -nr 's/^WM_CLASS.*\, \"(.*)\".*/\1/p')
-        debug "app is $app"
         window_title=$(xprop -id ${stat} | sed -nr 's/^WM_ICON_NAME.*\"(.*)\".*/\1/p')
-        debug "title is $window_title"
     fi
 
     time=$(date +"[%j] | %H:%M")
