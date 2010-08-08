c88caf198ffcfbab532fec04818393c76e1c5da4
Michael Nishizawa <michael.nishizawa@nishizawas.net>
2010-08-08 14:27:19 -0400

    updates

--- incubator/worklog.sh
+++ incubator/worklog.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Worklog polls the system for information on what you are actively working on at any given time.
-#  The script dumps output to STDOUT so it can be redirected to a log file or to the screen
 #
 #  DEPENDENCIES
 #    1. dbus - for the interface to gnome-screensaver
@@ -10,8 +9,43 @@
 #
 
 
-POLL_FREQUENCY=5
+export host=$(hostname)
+export output_dir="${HOME}/.worklog"
+export LOGFILE="${output_dir}/worklog-${host}-`date +%Y`"
+export POLL_FREQUENCY=5
+
+function log
+{
+    app=$1
+    shift
+    time=$(date +"%j - %H:%M:%S")
+    echo "| $time | $(hostname) | $app | $*" >> $LOGFILE
+}
+
+function check_ignores
+{
+    # Ignores tell the script what status to log when encountering a specific application
+    #  This matches on the application name
+    if [ -e ~/.worklog/ignores ]; then
+        #stub for later
+        #IGNORED=0
+        echo ""
+    fi
+
+    IGNORED=1
+}
+
+# Traps handle what to log in the event of signals
+trap 'log "session_end ${USER}" && exit 0' 1 2 3 
+trap 'log "system_shutdown ${USER}" && exit 0' 15
+
+# Initialize user directory if one doesn't exist
+if [ ! -e $output_dir ]; then
+    mkdir $output_dir
+fi
+
 stat='0'
+IGNORED=1
 
 while [ true ]; do
     last_stat=$stat
@@ -38,8 +72,14 @@ while [ true ]; do
         window_title=$(xprop -id ${stat} | sed -nr 's/^WM_ICON_NAME.*\"(.*)\".*/\1/p')
     fi
 
-    time=$(date +"[%j] | %H:%M")
-    echo "$time | $app | $window_title"
+    check_ignores $window_title
+    if [ 0 -eq $IGNORED ]; then
+        app="idle"
+        window_title=""
+        IGNORED=1
+    fi
+
+    log $app $window_title
 
     sleep $POLL_FREQUENCY
 done
